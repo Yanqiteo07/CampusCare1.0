b043c20127e9fc84f50dc165625f236c9be9cd0f
simyy-desu <simyy-wm25@student.tarc.edu.my>
2026-09-13 21:10:53 +0800

    Updated Report model ID type to String

--- app/src/main/java/com/example/campuscare10/model/Report.kt
+++ app/src/main/java/com/example/campuscare10/model/Report.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Report(
-    val id: Long? = null,
+    val id: String? = null,
     val created_at: String? = null,
     val category: String,
     val location: String,
